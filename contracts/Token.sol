b44a3081c0aafb22e07dffc68675fe37af308d8b
ASSnonymus <assnonymus@gmail.com>
2021-03-31 15:48:29 +0200

    Added interfaces and libraries from OpenZeppelin

---
+++ contracts/Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract Token is IBEP20, Ownable {
+
+    using SafeMath for uint256;
+    using Address for address;
+
+    
+    
+}
