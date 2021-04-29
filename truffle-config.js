54ad048925c6974e4cac8518ac84ab58113441d9
ASSnonymus <assnonymus@gmail.com>
2021-04-29 12:33:05 +0200

    Uploaded truffle-config.js

---
+++ truffle-config.js
@@ -0,0 +1,43 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { mnemonic, API_KEY_BSC_SCAN } = require('./secrets.json');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard BSC port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-2-s3.binance.org:8545`),
+      network_id: 97,
+      gas: 8000000,
+      gasPrice: 20000000000,
+      confirmations: 1,    
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
+  },
+  api_keys: {
+    bscscan: API_KEY_BSC_SCAN
+  },
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.0"
+    }
+  },
+
+  plugins: [
+    'truffle-plugin-verify',
+    'truffle-contract-size'
+  ],
+  api_keys:{
+    bscscan: API_KEY_BSC_SCAN
+  }
+};
